a9fb6214db8272ea759c13092970cb5a06503eb3
liuzunrui <liuzunrui@163.com>
2018-03-05 23:00:48 +0800

    增加模版

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
-from selenium import webdriver
 import unittest
+import time
 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class NewVisionTest(unittest.TestCase):
     def setUp(self):
@@ -12,6 +14,16 @@ class NewVisionTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')
+        time.sleep(5)
+        inputbox.send_keys(Keys.ENTER)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table")
         self.fail('Finish the test')
 
 
