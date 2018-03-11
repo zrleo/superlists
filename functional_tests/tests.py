862bf025974397ba21884db8ec67d74d9465cd63
liuzunrui <liuzunrui@163.com>
2018-03-11 23:15:27 +0800

    lzr_add_form

---
+++ functional_tests/tests.py
@@ -0,0 +1,55 @@
+import unittest
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from django.test import LiveServerTestCase
+
+
+class NewVisionTest(LiveServerTestCase):
+    def setUp(self):
+        self.browser  = webdriver.Chrome("driver/chromedriver")
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        time.sleep(10)
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get(self.live_server_url)
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Buy peacock feathers')      
+        inputbox.send_keys(Keys.ENTER)
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')  # unittest的辅助函数
+
+        table = self.browser.find_element_by_id('id_list_table')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.fail('Finish the test')
+        self.browser.quit()
+        self.browser = webdriver.Chrome("driver/chromedriver")
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assetNotIn('Buy peacock feathers', page_text)
+        self.assetNotIn('make a fly', page_text)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'lists.views.home_page', name='home'),
-
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
     url(r'^admin/', include(admin.site.urls)),
 ]
